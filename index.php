3f0e1c0193a23569fc859fc10a6e8a17c11a5a1a
hitek13 <13planetas.enanos@gmail.com>
2017-01-21 18:34:37 +0100

    Añadidas las bibliotecas de Bootstrap y JQuery

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>¡Hola, Mundo!</h1>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <style>
+    </style>
+  </head>
+  <body>
+      <h1>Hello World!</h1>
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
 </html>
